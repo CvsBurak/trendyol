a9676bcb66214bd64bf355c3e850267c5cd8055b
CvsBurak <burak-cavusoglu@hotmail.com>
2020-11-26 16:32:44 +0300

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -7,7 +7,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import ActionChains
 
 import pytest
+from pytest_html import extras
 
+
+driver = None
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -46,3 +49,24 @@ def setup(request):
     request.cls.driver = driver
     yield
     driver.close()
+    
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == 'setup':
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_")+".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="widht:304px;height:228px;" onclick="window.open(this.src)" align="right"/><div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+def _capture_screenshot(name):
+    driver.get_screenshot_as_file(name)
