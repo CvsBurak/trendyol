021b70ee22ea8c85e69a419590b462f5f21c1755
CvsBurak <burak-cavusoglu@hotmail.com>
2020-11-26 15:45:52 +0300

    Update BaseClass.py

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -3,6 +3,15 @@ import pytest
 
 @pytest.mark.usefixtures("setup")
 class BaseClass:
-    pass
 
+    def getLogger(self):
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
+        fileHandler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
 
+        logger.addHandler(fileHandler)
+
+        logger.setLevel(logging.DEBUG)
+        return logger
