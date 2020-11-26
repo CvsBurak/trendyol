d525dddaa8bace7875462808e3727edb33b9cbe9
CvsBurak <burak-cavusoglu@hotmail.com>
2020-11-26 14:54:47 +0300

    Update test_login.py

--- test/test_login.py
+++ test/test_login.py
@@ -1,5 +1,6 @@
 from utilities.BaseClass import BaseClass
 from pageObjects.HomePage import HomePage
+from TestData.LoginData import LoginData
 
 from time import sleep
 
@@ -11,9 +12,13 @@ class TestLogin(BaseClass):
         home = HomePage(self.driver)
         login = home.loginPageClick()
         sleep(2)
-        login.getMail().send_keys()
-        login.getPass().send_keys()
+        login.getMail().send_keys(getData["mail"])
+        login.getPass().send_keys(getData["password"])
         login.loginButton().click()
 
         sleep(2)
         assert home.isLoggedIn().is_displayed , "Login Failed"
+    
+    @pytest.fixture(params=LoginData.login_data)
+    def getData(self, request):
+        return request.param
